d1d944c332956fc53f3eb359886047d2ae17f161
maleckicoa <mihajlovic.aleksa@gmail.com>
2023-10-22 15:06:54 +0200

    Docker image done. Just left to finish the README file

--- PD_model_train.py
+++ PD_model_train.py
@@ -16,105 +16,6 @@ pd.set_option('display.max_rows', 1000)
 pd.options.display.float_format = '{:.5f}'.format
 
 
-class WoeEncode:
-    def __init__(self, df):
-
-        # self.data_path = data_path
-        # self.df = pd.read_csv(self.data_path, sep = ";")
-        self.df = df
-        self.df['has_paid'] = self.df['has_paid'].astype(int)
-        self.df_nna = self.df[self.df['default'].notna()]
-
-        # self.df_na = self.df[self.df['default'].isna()]
-
-        self.df_num_cat = self.num_cat()
-        self.df_nna_bin, self.num_vars = self.num_bin()
-
-        self.df_nna_bin_woe, self.woe_cols = self.WOE()
-        self.lookup_table = self.lookup()
-        # self.df_na_bin_woe = self.na_bin_WOE()
-
-    def num_cat(self):
-        nan_col_list = self.df_nna.columns[self.df_nna.isnull().any()]
-        columns = self.df_nna.columns
-        unique_vals = [self.df_nna[col].unique() for col in columns]
-        unique_counts = [len(vals) for vals in unique_vals]
-
-        df_num_cat = pd.DataFrame({
-            'Columns': columns,
-            'Unique Val': unique_vals,
-            'Unique Val Count': unique_counts
-        })
-
-        df_num_cat['Variable Type'] = np.where(df_num_cat['Unique Val Count'] < 15, 'Categorical', 'Numerical')
-        df_num_cat.loc[df_num_cat['Columns'].isin(
-            ['merchant_category', 'merchant_group', 'name_in_email']), 'Variable Type'] = 'Categorical'
-        df_num_cat.loc[df_num_cat['Columns'].isin(['uuid', 'default']), 'Variable Type'] = '/'
-        df_num_cat['Has NA'] = np.where(df_num_cat['Columns'].isin(nan_col_list), 'NA', 'OK')
-        return df_num_cat
-
-    def num_bin(self):
-        df_nna_bin = self.df_nna.copy()
-        num_vars = list(self.df_num_cat['Columns'].loc[self.df_num_cat['Variable Type'] == 'Numerical'])
-        for i in num_vars:
-            df_nna_bin[i] = pd.cut(df_nna_bin[i], bins=20, precision=0)
-        df_nna_bin[self.df_nna.columns[2:]] = df_nna_bin[self.df_nna.columns[2:]].astype(str)
-        return df_nna_bin, num_vars
-
-    def WOE(self):
-        data_set = self.df_nna_bin
-        data_targets = data_set['default']
-        data_features = data_set.drop(['default', 'uuid'], axis=1)
-        columns = [col for col in data_features.columns]
-        woe_encoder = ce.WOEEncoder(cols=columns)
-        woe_encoded_data = woe_encoder.fit_transform(data_features[columns], data_targets).add_suffix('_woe')
-        return data_set.join(woe_encoded_data), list(woe_encoded_data.columns)
-
-    def lookup(self):
-        col_name, lower_bound, upper_bound, orig_value = [], [], [], []
-        for i in self.num_vars:
-            for j in self.df_nna_bin[i].unique():
-                if j != 'nan':
-                    col_name.append(i)
-                    bounds = j.strip('()[]').split(',')
-                    lower_bound.append(float(bounds[0]))
-                    upper_bound.append(float(bounds[1]))
-                    orig_value.append(j)
-        lookup_table = pd.DataFrame(
-            {'col_name': col_name, 'lower_bound': lower_bound, 'upper_bound': upper_bound, 'orig_value': orig_value})
-        return lookup_table
-
-    def map_range(self, col_name, value):
-        if pd.isna(value):
-            return 'nan'
-
-        sub_table = self.lookup_table.loc[self.lookup_table['col_name'] == col_name]
-        try:
-            return str(sub_table["orig_value"][
-                           (sub_table["lower_bound"] <= value) & (sub_table["upper_bound"] > value)].values[0])
-        except:
-            return 'nan'
-
-    def na_bin_WOE(self, df_na):
-
-        df_na_bin = df_na.copy()
-
-        for i in self.num_vars:
-            df_na_bin[i] = df_na_bin[i].apply(lambda x: self.map_range(i, x))
-
-        df_na_bin[df_na.columns[2:]] = df_na_bin[self.df_nna.columns[2:]].astype(str)
-        df_nna_bin_cols = list(self.df_nna_bin.columns)[2:]
-
-        woe_mapping = pd.DataFrame({'Cols': df_nna_bin_cols, 'Cols_WOE': self.woe_cols})
-        df_na_bin_woe = df_na_bin.copy()
-
-        for i in range(0, len(woe_mapping)):
-            df_map = self.df_nna_bin_woe[[str(woe_mapping.Cols[i]), str(woe_mapping.Cols_WOE[i])]].drop_duplicates()
-            df_na_bin_woe = pd.merge(df_na_bin_woe, df_map, how="left", on=[str(woe_mapping.Cols[i])])
-
-        return df_na_bin_woe
-
-
 class TrainValTest:
     def __init__(self, path, val_prop, test_prop):
         """
@@ -127,7 +28,7 @@ class TrainValTest:
         """
 
         _df = pd.read_csv(path, sep=";")
-        _df = _df[~_df['default'].isna()]#.head(1000)
+        _df = _df[~_df['default'].isna()]  # .head(1000)
 
         _val_size = int(val_prop * len(_df))
         _test_size = int(test_prop * len(_df))
@@ -162,10 +63,181 @@ class TrainValTest:
         self.test_y = _test_y
 
 
-class Model:
-    def __init__(self, dataset_path, train_prop, test_prop, grid):
+class WoeEncode:
+    def __init__(self, df):
 
-        self.data = TrainValTest("./dataset.csv", 0.1, 0.1)
+        """
+        The WoeEncode class receives a dataset to encode.
+        The class should be initialized with a train set.
+        The class creates an object which holds the encoded train set,
+        as well as methods to encode the validation and test set.
+
+        Parameters:
+        - df (pd.DataFrame): the training dataset
+        """
+
+        self.df = df
+        self.df['has_paid'] = self.df['has_paid'].astype(int)
+        self.df_nna = self.df[self.df['default'].notna()]
+
+        # self.df_na = self.df[self.df['default'].isna()]
+
+        self.df_num_cat = self.num_cat()
+        self.df_nna_bin, self.num_vars = self.num_bin()
+
+        self.df_nna_bin_woe, self.woe_cols = self.WOE()
+        self.lookup_table = self.lookup()
+        # self.df_na_bin_woe = self.na_bin_WOE()
+
+    def num_cat(self):
+        """
+        The num_cat method separates the dataset into categorical and numerical features.
+        This is done because the numerical features must be binned before encoding.
+        The method returns a summary table which classifies the features into either numerical or categorical
+
+        Parameters:
+        -none
+        """
+        nan_col_list = self.df_nna.columns[self.df_nna.isnull().any()]
+        columns = self.df_nna.columns
+        unique_vals = [self.df_nna[col].unique() for col in columns]
+        unique_counts = [len(vals) for vals in unique_vals]
+
+        df_num_cat = pd.DataFrame({
+            'Columns': columns,
+            'Unique Val': unique_vals,
+            'Unique Val Count': unique_counts
+        })
+
+        df_num_cat['Variable Type'] = np.where(df_num_cat['Unique Val Count'] < 15, 'Categorical', 'Numerical')
+        df_num_cat.loc[df_num_cat['Columns'].isin(
+            ['merchant_category', 'merchant_group', 'name_in_email']), 'Variable Type'] = 'Categorical'
+        df_num_cat.loc[df_num_cat['Columns'].isin(['uuid', 'default']), 'Variable Type'] = '/'
+        df_num_cat['Has NA'] = np.where(df_num_cat['Columns'].isin(nan_col_list), 'NA', 'OK')
+        return df_num_cat
+
+    def num_bin(self):
+        """
+        The method num_bin utilizes the df_num_cat object and returns a dataframe object,
+        whose numerical features have been binned into 20 bins each.
+        The method also returns a list of all numerical columns/features.
+
+        Parameters:
+        -none
+        """
+        df_nna_bin = self.df_nna.copy()
+        num_vars = list(self.df_num_cat['Columns'].loc[self.df_num_cat['Variable Type'] == 'Numerical'])
+        for i in num_vars:
+            df_nna_bin[i] = pd.cut(df_nna_bin[i], bins=20, precision=0)
+        df_nna_bin[self.df_nna.columns[2:]] = df_nna_bin[self.df_nna.columns[2:]].astype(str)
+        return df_nna_bin, num_vars
+
+    def WOE(self):
+        """
+        The WOE class, encodes the train dataset, whose numerical features have previously been binned
+
+        Parameters:
+        -none
+        """
+        data_set = self.df_nna_bin
+        data_targets = data_set['default']
+        data_features = data_set.drop(['default', 'uuid'], axis=1)
+        columns = [col for col in data_features.columns]
+        woe_encoder = ce.WOEEncoder(cols=columns)
+        woe_encoded_data = woe_encoder.fit_transform(data_features[columns], data_targets).add_suffix('_woe')
+        return data_set.join(woe_encoded_data), list(woe_encoded_data.columns)
+
+    def lookup(self):
+
+        """
+        The lookup method serves to define a lookup table which is a part of the mapping table,
+        later used to WOE encode the validation and train dataset.
+
+        Parameters:
+        -none
+        """
+        col_name, lower_bound, upper_bound, orig_value = [], [], [], []
+        for i in self.num_vars:
+            for j in self.df_nna_bin[i].unique():
+                if j != 'nan':
+                    col_name.append(i)
+                    bounds = j.strip('()[]').split(',')
+                    lower_bound.append(float(bounds[0]))
+                    upper_bound.append(float(bounds[1]))
+                    orig_value.append(j)
+        lookup_table = pd.DataFrame(
+            {'col_name': col_name, 'lower_bound': lower_bound, 'upper_bound': upper_bound, 'orig_value': orig_value})
+        return lookup_table
+
+    def map_range(self, col_name, value):
+        """
+        map_range method handles the cases of outliers when performing the WOE encoding of the
+        validation/test dataset. The outliers are values which do not fall within the defined
+        ranges of the lookup table.
+
+        This occurs because the outliers belong to the test dataset, but the lookup table was created
+        upon the WOE encoding of the train dataset.
+
+        The map_range method upgrades the lookup table to handle the outlier values.
+
+        Parameters:
+        -col_name (str): names of the numerical columns/features
+        -value (str): upper or lower range value of the train dataset feature
+
+        """
+        if pd.isna(value):
+            return 'nan'
+
+        sub_table = self.lookup_table.loc[self.lookup_table['col_name'] == col_name]
+        try:
+            return str(sub_table["orig_value"][
+                           (sub_table["lower_bound"] <= value) & (sub_table["upper_bound"] > value)].values[0])
+        except:
+            return 'nan'
+
+    def na_bin_WOE(self, df_na):
+        """
+        The na_bin_WOE method encodes the valudation/test dataset.
+        The method basically maps the original feature values into their WOE values using a mapping table.
+
+        Parameters:
+        -df_na (pd. Dataframe):the validation/test dataset
+        """
+
+        df_na_bin = df_na.copy()
+
+        for i in self.num_vars:
+            df_na_bin[i] = df_na_bin[i].apply(lambda x: self.map_range(i, x))
+
+        df_na_bin[df_na.columns[2:]] = df_na_bin[self.df_nna.columns[2:]].astype(str)
+        df_nna_bin_cols = list(self.df_nna_bin.columns)[2:]
+
+        woe_mapping = pd.DataFrame({'Cols': df_nna_bin_cols, 'Cols_WOE': self.woe_cols})
+        df_na_bin_woe = df_na_bin.copy()
+
+        for i in range(0, len(woe_mapping)):
+            df_map = self.df_nna_bin_woe[[str(woe_mapping.Cols[i]), str(woe_mapping.Cols_WOE[i])]].drop_duplicates()
+            df_na_bin_woe = pd.merge(df_na_bin_woe, df_map, how="left", on=[str(woe_mapping.Cols[i])])
+
+        return df_na_bin_woe
+
+
+class Model:
+    def __init__(self, dataset_path, val_prop, test_prop, grid):
+
+        """
+        The class Model initializes the TrainValTest and the WoeEncode data preparation classes,
+        and creates the trains the XGB model using grid search cross-validation.
+
+        Parameters:
+        - dataset_path(str): Loacation of the complete dataset
+        - val_prop (float): Proportion of data for the validation set
+        - test_prop (float): Proportion of data for the test set
+        - grid (dict): Dictionary of hyper parameters for training the model
+
+        """
+
+        self.data = TrainValTest(dataset_path, val_prop, test_prop)
         self.obj = WoeEncode(self.data.train)
 
         _val_woe_x = self.obj.na_bin_WOE(self.data.val)
@@ -177,7 +249,7 @@ class Model:
 
         self.val_data = self.data.val
         self.val_X = _val_woe_x[self.obj.woe_cols]
-        self.val_y = self.data.val_y.values
+        self.val_y = self.data.val_y
 
         self.test_data = self.data.test
         self.test_X = _test_woe_x[self.obj.woe_cols]
@@ -192,12 +264,22 @@ class Model:
         print("Model is trained")
         print("Best parameters found: ", self.grid_search.best_params_)
 
-    def test_set_performance(self):
+    def val_set_performance(self):
 
-        y_pred_prob = self.grid_search.best_estimator_.predict_proba(self.test_X.values)[:, 1]
-        y_pred = self.grid_search.best_estimator_.predict(self.test_X.values)
+        """
+        This methods runs the XGB classifer on the validation set and returns the performance
+        results together with the confusion matrix.
 
-        y_true = self.test_y.values.tolist()
+        The performance resuls can be used to furter optimize the model hyper-parameters.
+
+        Parameters:
+        -none
+        """
+
+        y_pred_prob = self.grid_search.best_estimator_.predict_proba(self.val_X.values)[:, 1]
+        y_pred = self.grid_search.best_estimator_.predict(self.val_X.values)
+
+        y_true = self.val_y.values.tolist()
 
         auc_roc = roc_auc_score(y_true, y_pred_prob)
         rec = recall_score(y_true, y_pred)
@@ -211,11 +293,18 @@ class Model:
 
         print(confusion_matrix(y_true, y_pred))
 
-
     def predict(self, loans_dict):
+        """
+        The predict method is applied within the dockerized application.
+        The method takes in one or more loans (either as a JSON file or as a curl post request)
+        and returns a dictionary of loan IDs and their probabilities of default
 
+        Parameters:
+        - loan_dict(dict/json):
+
+        """
         appended_data = []
-        for i in range(0,len(loans_dict)):
+        for i in range(0, len(loans_dict)):
             df = pd.DataFrame([loans_dict[i].dict()])
             appended_data.append(df)
 
@@ -233,7 +322,55 @@ class Model:
 
         res_dict = {key: value for key, value in zip(loan_id, probability_of_default)}
         return res_dict
+
+    def predict_ipynb(self, loans_dict):
+        """
+        The predict_ipynb method is applied within the Jupyter Notebook.
+        The method takes in one or more loans (via a path string to JSON file)
+        and returns a dictionary of loan IDs and their probabilities of default
+
+        Parameters:
+        - loan_dict(str): path location of a JSON file where the loans are defined
+        """
+        if type(loans_dict) == str:
+            f = open(loans_dict)
+            data = json.load(f)
+        else:
+            data = loans_dict.copy()
+
+        self.loan_frame = pd.DataFrame(data)
+
+        loans_woe_x = self.obj.na_bin_WOE(self.loan_frame)
+        loans_X = loans_woe_x[self.obj.woe_cols].values
+
+        y_pred_prob = self.grid_search.best_estimator_.predict_proba(loans_X)[:, 1]
+
+        loan_id = list(self.loan_frame['uuid'])
+        probability_of_default = list(y_pred_prob)
+        probability_of_default = [str(x) for x in probability_of_default]
+
+        res_dict = {key: value for key, value in zip(loan_id, probability_of_default)}
+
+        return res_dict
+
     def generate_loans(self, test_size=10, file_name="./loan.json"):
+
+        """
+        The generate_loans method generates a JSON file holding a dictionary of loans,
+        for which the user can calculate the probability of default by passing
+        this JSON file as in input to the predict/predich_ipynb method.
+
+        The JSON file is made up of random loans from the test dataset, the idea is to
+        allow the user to quickly generate test loans.
+
+        Note that the user can also manualy specify a customized loan and pass it to
+        the prediction method within a JSON file.
+
+        Parameters:
+        -test_size (int): number of test loans within the JSON file
+        -file name (str): string path to the JSON file
+
+        """
         random_loans = random.sample(range(1, len(self.data.test)), test_size)
         test_dict = self.data.test.iloc[random_loans]  # .to_dict()
         # print(test_dict)
@@ -242,9 +379,7 @@ class Model:
             test_dict.to_json(file, orient='records', indent=4)
 
 
-
 if __name__ == '__main__':
-
     param_grid = {
         'learning_rate': [0.01],
         'max_depth': [8, 9, 10],
@@ -255,5 +390,8 @@ if __name__ == '__main__':
         # 'reg_lambda': [0.1, 0.5],
     }
 
+    # Instantiate & train the Model object
     mod_dump = Model("./dataset.csv", 0.1, 0.1, param_grid)
+
+    # Serialize the trained Model object - done to avoid retraining of the model at every application start
     joblib.dump(mod_dump, './mod.pkl')
---
+++ README.md
@@ -0,0 +1,53 @@
+What is the application about?
+
+malekicoa/pd_pred is a Dockerized application that returns the probability of default for a set of given loans
+The application is based on a real dataset from one large European fintech company (see: dataset.csv).
+
+
+How to use the pd_pred application?
+
+- Install and run the Docker application
+- navigate to the project folder and run "docker run -ti maleckicoa/pd_pred" - to load and run the image from Docker Hub
+- open a new terminal, run "docker ps", then copy the <container ID> of the running pd_pred container
+- run "docker exec -it <container ID> sh" to open the shell of the running pd_pred container
+- paste the *curl POST request (which holds loan information) into the open container shell,
+  and it will return the loan ID together with loan default probabilities
+
+* Note that the application supports:
+  -curl requests which hold a dictionary with loan information (see example in curl_request.txt)
+  -curl requests which reference a JSON file with loan information (see request example in curl_file.txt,
+  see examples of JSON files: loan1.json, loan5.json, loan20.json)
+
+
+How does the application work?
+
+The pd_pred application consists of 2 python scripts.
+
+The main.py script imports the PD_model_train.py script and starts a FastAPI application.
+FastAPI application serves to receive loans information as curl POST requests and to return
+the default probabilities it receives from the model object.
+
+The PD_model_train.py holds 3 classes which respectively:
+- split the dataset into train/val/test subsets (TrainValTest)
+- encode the data subsets using Weight of Evidence encoding (WoeEncode)
+- train the XG-boost classifier and return probability od default (Model)
+
+Note that the model object is pretrained and serialized (mod.pkl file).
+At runtime the model object is imported and the FastAPI application passes the loan information
+to the model object, which in return provides the default probabilities. The model object can be re-trained,
+this is done by directly running the PD_model.train.py script as __main__.
+
+Useful Info
+The repository also has a PD EDA & Model Run.ipynb jupyter notebook with the exploratory data analysis
+
+If the user wishes to make changes to the source code of the container, follow the steps:
+- create a new local directory
+- start an existing maleckicoa/pd_pred image container (or run a new container)
+- copy the container files into the local folder "docker cp <container ID>: /usr/src/app/. /my_local_directory"
+- close the container
+- run the pd_pred_image (this will make a new container) and attach volumes to the local folder
+ "docker run -v /my_local_directory:/usr/src/app malckicoa/pd_pred"
+- you can now make changes to the source code in your local directory. The changes will be reflected in
+  this particular container, even if you stop it and start it again.
+
+
